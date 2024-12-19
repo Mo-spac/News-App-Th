0667140c25ea155c3d005fe03fdcebc8728f333a
Mohammed Samy <mom118580@gmail.com>
2024-12-19 15:30:08 +0200

    Future Builder part 1

--- lib/widget/news_list_view_builder.dart
+++ lib/widget/news_list_view_builder.dart
@@ -4,43 +4,42 @@ import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widget/news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
-  const NewsListViewBuilder({super.key});
+class NewsListViewBuilder extends StatelessWidget {
+  // List<ArticleModel> articles = [];
+  // bool isLoading = true;
 
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
+  // @override
+  // void initState() {
+  //   getGeneralNews();
+  //   super.initState();
+  // }
 
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    getGeneralNews();
-    super.initState();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(dio: Dio()).getNews();
-    isLoading = false;
-    setState(() {});
-  }
+  // Future<void> getGeneralNews() async {
+  //   articles = await NewsService(dio: Dio()).getNews();
+  //   isLoading = false;
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? SliverToBoxAdapter(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          )
-        : articles.isNotEmpty
-            ? NewsListView(articles: articles)
-            : SliverToBoxAdapter(
-                child: Center(
-                  child: Text("oops There was an Error, try later!"),
-                ),
-              );
+    return FutureBuilder(
+        future: NewsService(dio: Dio()).getNews(),
+        builder: (context, snapshot) {
+          return NewsListView(articles: snapshot.data ?? []);
+        });
+
+    // return isLoading
+    //     ? SliverToBoxAdapter(
+    //         child: Center(
+    //           child: CircularProgressIndicator(),
+    //         ),
+    //       )
+    //     : articles.isNotEmpty
+    //         ? NewsListView(articles: articles)
+    //         : SliverToBoxAdapter(
+    //             child: Center(
+    //               child: Text("oops There was an Error, try later!"),
+    //             ),
+    //           );
   }
 }
